7ac62c7a0ca03f0df1763cc807f8ced13d3c0253
JedrzejZygadlo <jedrzejzygadlo@gmail.com>
2023-02-13 23:20:35 +0100

    add-currencies-icons

---
+++ src/components/icons/LTC.vue
@@ -0,0 +1,7 @@
+<template>
+  <g fill="none" fill-rule="evenodd"
+    ><circle cx="16" cy="16" r="16" fill="#BFBBBB" /><path
+      fill="#FFF"
+      d="M10.427 19.214L9 19.768l.688-2.759 1.444-.58L13.213 8h5.129l-1.519 6.196 1.41-.571-.68 2.75-1.427.571-.848 3.483H23L22.127 24H9.252z"
+  /></g>
+</template>
